ffbcf07de9e05f1228e9e5154c358047822af682
Yosi0530 <93134160+Yosi0530@users.noreply.github.com>
2023-09-16 18:59:46 +0900

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '「赤墓ビーチ」 TWICE  Dance The Night Away',
-            image: 'https://thebiaslistcom.files.wordpress.com/2018/07/twice-dance-the-night-away.jpg',
+            image: '',
             description: 'こちらは沖縄県今帰仁村にある赤墓(あかばか)ビーチです。TWICEの代表的な夏の曲である「Dance The Night Away」は実は沖縄県で撮影されていました！沖縄県の青い海や綺麗な砂浜をバックに踊っているのが印象的ですね！沖縄県にある数ある有名ビーチには数えられていませんが、K-POPファンであるならぜひ一度ここに訪れて本人たちと同じ画角でとってみたいものですね！　車で訪れるのがおすすめです。 <br> YouTube:https://youtu.be/Fm5iP0S1z9w?feature=shared <br>  <br> 住所：沖縄県国頭郡今帰仁村諸志 <br> 駐車場：無料（約7台）有料（約5台）',
             location: {
                 center: [127.94124, 26.70487],
@@ -75,8 +75,9 @@ var config = {
             alignment: 'left',
             hidden: false,
 	title: '「読谷村残波岬」 BTS  Let Go',
-            image: 'https://www.surairu-okinawa.com/wp-content/uploads/2023/02/87716fbeba481e9e9f8d8f9f05e8834f-1536x1040.jpg',
-            description: 'こちらは沖縄県本島中部に位置する読谷村残波岬というスポットです。BTSの日本オリジナルアルバム「Face Yourself」に収録されている日本語曲「Let Go」のMV撮影地もなんと沖縄県です！残波岬の道で撮影されたメンバーのVのシーンを真似して写真を撮ることがARMY(ファン)の中でトレンドとなっています。すぐ近くの残波岬公園にはバスケットボールやサッカー、テニスができる施設があるので残波岬に訪れた際はそちらにも足を運んでみてはいかがでしょうか。 <br>  <br> 住所：沖縄県中頭郡読谷村宇座1861 <br> 駐車場：約100台(無料)',
+            image: 'https://user-images.githubusercontent.com/93134160/268441326-b2f37054-00e3-474e-9aec-da025f49e0e9.png',
+            description: 'こちらは沖縄県本島中部に位置する読谷村残波岬というスポットです。BTSの日本オリジナルアルバム「Face Yourself」に収録されている日本語曲「Let Go」のMV撮影地もなんと沖縄県です！残波岬の道で撮影されたメンバーのVのシーンを真似して写真を撮ることがARMY(ファン)の中でトレンドとなっています。すぐ近くの残波岬公園にはバスケットボールやサッカー、テニスができる施設があるので残波岬に訪れた際はそちらにも足を運んでみてはいかがでしょうか。 <br>  <br> 住所：沖縄県中頭郡読谷村宇座1861 <br> 駐車場：約100台(無料) <br>  <br> image by pika_sky under a [CC BY-SA](https://creativecommons.org/licenses/by-sa/4.0) license
+©︎ [OpenStreetMap contributors](https://www.openstreetmap.org/copyright)',
             location: {
                 center: [127.72054, 26.43748],
 		zoom: 16.31,
@@ -94,8 +95,9 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '「名鉄沿線」　NewJeans  Hurt (250 Remix) Special Video',
-            image: 'https://pbs.twimg.com/media/FlE0HLgaYAQu54o?format=jpg&name=large',
-            description: '今話題の5人組ガールズグループ New Jeansも実は日本で撮影しており、ファンからは驚きの声がでています！愛知県名古屋市に通っている名鉄や庄内川付近で撮影されていました。沖縄のビーチや岬とは打って変わって、アクセスの良い街中で撮影されているため、ぜひ訪れてみてはいかがでしょうか。MVもノスタルジックな雰囲気で景色とマッチしています！ <br> https://youtu.be/uONSk48LW1A?feature=shared <br>  <br> 愛知県名古屋市西区枇杷島町2丁目25-8付近 <br> 最寄駅：東枇杷島駅(犬山線、名鉄名古屋本線)',
+            image: 'https://user-images.githubusercontent.com/93134160/268436057-3de6e612-5e61-45b5-8678-ddb573921e53.png',
+            description: '今話題の5人組ガールズグループ New Jeansも実は日本で撮影しており、ファンからは驚きの声がでています！愛知県名古屋市に通っている名鉄や庄内川付近で撮影されていました。沖縄のビーチや岬とは打って変わって、アクセスの良い街中で撮影されているため、ぜひ訪れてみてはいかがでしょうか。MVもノスタルジックな雰囲気で景色とマッチしています！ <br> https://youtu.be/uONSk48LW1A?feature=shared <br>  <br> 愛知県名古屋市西区枇杷島町2丁目25-8付近 <br> 最寄駅：東枇杷島駅(犬山線、名鉄名古屋本線) <br>  <br> image by mudb under a [CC BY-SA](https://creativecommons.org/licenses/by-sa/4.0) license
+©︎ [OpenStreetMap contributors](https://www.openstreetmap.org/copyright)',
             location: {
               center: [136.86846, 35.19177],
 		zoom: 17.22,
@@ -113,8 +115,9 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '「27 MCM」 Stray Kids  There',
-            image: 'https://anotherview-location.com/wp-content/uploads/2023/03/0001-1.jpg',
-            description: '人気ボーイズグループ「Stray Kids」の日本語曲である「There」は東京・横浜周辺で撮影されていました。 <br> 東京都渋谷区にある「27 MCM」というお好み焼き屋は、メンバーのメインラッパー「ハン」の撮影地です。渋谷駅から徒歩約10分なため、渋谷に訪れた際はぜひ立ち寄ってみてはいかがでしょうか。本人と同じ席に座れたらラッキー！！ <br>  <br> 住所：東京都渋谷区鶯谷町６−５ <br> 営業時間：17:00~25:00',
+            image: 'https://user-images.githubusercontent.com/93134160/268436358-b034ed47-0752-4e17-8d39-a97bd3b1e743.png',
+            description: '人気ボーイズグループ「Stray Kids」の日本語曲である「There」は東京・横浜周辺で撮影されていました。 <br> 東京都渋谷区にある「27 MCM」というお好み焼き屋は、メンバーのメインラッパー「ハン」の撮影地です。渋谷駅から徒歩約10分なため、渋谷に訪れた際はぜひ立ち寄ってみてはいかがでしょうか。本人と同じ席に座れたらラッキー！！ <br>  <br> 住所：東京都渋谷区鶯谷町６−５ <br> 営業時間：17:00~25:00 <br>  <br> image by mzaki under a [CC BY-SA](https://creativecommons.org/licenses/by-sa/4.0) license
+©︎ [OpenStreetMap contributors](https://www.openstreetmap.org/copyright)',
             location: {
                center: [139.70269, 35.65412],
 		zoom: 16.95,
@@ -132,8 +135,9 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '「YOKOHAMA AIR CABIN」  Stray Kids  There',
-            image: 'https://anotherview-location.com/wp-content/uploads/2023/03/0007.jpg',
-            description: 'メンバーのメインボーカル「スンミン」がMVの中で訪れた横浜市桜木町にあるエアキャビン。観光スポットとしても大変人気な施設で、横浜の市内を一望できます。 <br>  <br> 住所：神奈川県横浜市中区桜木町１丁目２００ <br> 営業時間：10:00~22:00',
+            image: 'https://user-images.githubusercontent.com/93134160/268436530-aaec0341-db34-403b-8b3f-3d2c976f9062.png',
+            description: 'メンバーのメインボーカル「スンミン」がMVの中で訪れた横浜市桜木町にあるエアキャビン。観光スポットとしても大変人気な施設で、横浜の市内を一望できます。 <br>  <br> 住所：神奈川県横浜市中区桜木町１丁目２００ <br> 営業時間：10:00~22:00 <br>  <br> image by mitz under a [CC BY-SA](https://creativecommons.org/licenses/by-sa/4.0) license
+©︎ [OpenStreetMap contributors](https://www.openstreetmap.org/copyright)',
             location: {
               center: [139.63147, 35.45150],
 		zoom: 16.23,
@@ -151,8 +155,9 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '「THE BAYS」  Stray Kids  There',
-            image: 'https://anotherview-location.com/wp-content/uploads/2023/03/0009.jpg',
-            description: 'メンバーのメインダンサーである「リノ」はMVで横浜市中区にある「THE BAYS」のテラスを訪れました。「THE BAYS」とは横浜DeNAベイスターズが提唱する横浜スポーツタウン構想として建てられた、Sports x Creativeをテーマにした複合施設です。おしゃれな空間が広がっているので中の方もぜひ入ってみてください。 <br>  <br> 住所；神奈川県横浜市中区日本大通３４',
+            image: 'https://user-images.githubusercontent.com/93134160/268436692-dae1f555-9c7d-40a6-adfc-32797d7a3460.png',
+            description: 'メンバーのメインダンサーである「リノ」はMVで横浜市中区にある「THE BAYS」のテラスを訪れました。「THE BAYS」とは横浜DeNAベイスターズが提唱する横浜スポーツタウン構想として建てられた、Sports x Creativeをテーマにした複合施設です。おしゃれな空間が広がっているので中の方もぜひ入ってみてください。 <br>  <br> 住所；神奈川県横浜市中区日本大通３４ <br>  <br> image by mura under a [CC BY-SA](https://creativecommons.org/licenses/by-sa/4.0) license
+©︎ [OpenStreetMap contributors](https://www.openstreetmap.org/copyright)',
             location: {
                 center: [139.64187, 35.44485],
 		zoom: 16.76,
@@ -170,8 +175,9 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '「横浜港大さん橋」  Stray Kids  There',
-            image: 'https://anotherview-location.com/wp-content/uploads/2023/03/0010.jpg',
-            description: 'Stray Kidsのメンバーで、低音ボイスとお顔が魅力的な「Felix」はみなとみらいの「横浜港大さん橋」に訪れています。ぜひ本人と同じ画角で写真を撮ってみてください！ <br>  <br> 住所：神奈川県横浜市中区海岸通１丁目1−４ <br>  <br> 今回ご紹介した曲「There」の撮影場所のほとんどは観光地かつアクセスの良い場所で撮られているため、東京都心部、横浜を訪れた際は立ち寄って本人たちの奇跡を辿ってみてはいかがでしょうか',
+            image: 'https://user-images.githubusercontent.com/93134160/268437145-e41f5341-563f-4abd-a049-01fc97efe729.png',
+            description: 'Stray Kidsのメンバーで、低音ボイスとお顔が魅力的な「Felix」はみなとみらいの「横浜港大さん橋」に訪れています。ぜひ本人と同じ画角で写真を撮ってみてください！ <br>  <br> 住所：神奈川県横浜市中区海岸通１丁目1−４ <br>  <br> image by mura under a [CC BY-SA](https://creativecommons.org/licenses/by-sa/4.0) license
+©︎ [OpenStreetMap contributors](https://www.openstreetmap.org/copyright) <br>  <br> 今回ご紹介した曲「There」の撮影場所のほとんどは観光地かつアクセスの良い場所で撮られているため、東京都心部、横浜を訪れた際は立ち寄って本人たちの奇跡を辿ってみてはいかがでしょうか',
             location: {
                 center: [139.64665, 35.45090],
 		zoom: 15.96,
